9bf8eaa788675a0aa94cf092538400614375d97d
Adam Retter <adam.retter@googlemail.com>
2020-08-23 14:06:03 +0200

    Further User API tests

--- src/test/java/com/fusiondb/studio/api/UserIT.java
+++ src/test/java/com/fusiondb/studio/api/UserIT.java
@@ -27,6 +27,7 @@ import static com.evolvedbinary.j8fu.tuple.Tuple.Tuple;
 import static com.fusiondb.studio.api.API.*;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.apache.http.HttpStatus.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -95,8 +96,10 @@ public class UserIT {
             when().
                     get(getApiBaseUri() + "/user/user" + userId).
             then().
-                    statusCode(SC_OK)
-            .extract();
+                    statusCode(SC_OK).
+            assertThat().
+                    body(matchesJsonSchemaInClasspath("user-schema.json")).
+            extract();
     }
 
     private void createUser(final String userId) {
